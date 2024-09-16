a3f45cebf278d19f4292f9207ed7a45ad50f4a8c
Abhishek Verma <abhishek.rmv@gmail.com>
2024-09-17 00:55:53 +0530

    Added base project

---
+++ Clock-SwiftUI/Clock_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clock_SwiftUIApp.swift
+//  Clock-SwiftUI
+//
+//  Created by Abhishek  on 17/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Clock_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
