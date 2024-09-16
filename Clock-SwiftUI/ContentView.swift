b60a922fff40861e5e3a59f680a6333d79a6ae81
Abhishek Verma <abhishek.rmv@gmail.com>
2024-09-17 01:06:06 +0530

    Dynamic size as per screen size

--- Clock-SwiftUI/ContentView.swift
+++ Clock-SwiftUI/ContentView.swift
@@ -10,26 +10,30 @@ struct ContentView: View {
     @State private var currentDate = Date()
     
     var body: some View {
-        ZStack {
-            // Clock background
-            Circle()
-                .fill(Color.white)
-                .overlay(
-                    Circle().stroke(Color.black, lineWidth: 4)
-                )
-                .frame(width: 300, height: 300)
+        GeometryReader { geometry in
+            let clockSize = min(geometry.size.width, geometry.size.height) * 0.8
             
-            // Hour Marks
-            ForEach(0..<12) { i in
-                Rectangle()
-                    .fill(Color.black)
-                    .frame(width: 2, height: 20)
-                    .offset(y: -150 + 10)
-                    .rotationEffect(.degrees(Double(i) * 30))
+            ZStack {
+                // Clock background
+                Circle()
+                    .fill(Color.white)
+                    .overlay(
+                        Circle().stroke(Color.black, lineWidth: 4)
+                    )
+                    .frame(width: clockSize, height: clockSize)
+                
+                // Hour Marks
+                ForEach(0..<12) { i in
+                    Rectangle()
+                        .fill(Color.black)
+                        .frame(width: clockSize * 0.01, height: clockSize * 0.1)
+                        .offset(y: -clockSize / 2 + clockSize * 0.05)
+                        .rotationEffect(.degrees(Double(i) * 30))
+                }
+                
+                // Hour hand
+                HandView(width: clockSize * 0.02, height: clockSize * 0.3)
             }
-            
-            // Hour hand
-            HandView(width: 6, height: 80)
         }
     }
 }
