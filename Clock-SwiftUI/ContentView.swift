0ca23784873f3984c0cca18c380e62a4d97851aa
Abhishek Verma <abhishek.rmv@gmail.com>
2024-09-17 01:00:01 +0530

    Added hour lines

--- Clock-SwiftUI/ContentView.swift
+++ Clock-SwiftUI/ContentView.swift
@@ -8,16 +8,25 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var currentDate = Date()
-
+    
     var body: some View {
         ZStack {
             // Clock background
             Circle()
                 .fill(Color.white)
                 .overlay(
-                    Circle().stroke(Color.black, lineWidth: 10)
+                    Circle().stroke(Color.black, lineWidth: 4)
                 )
                 .frame(width: 300, height: 300)
+            
+            // Hour Marks
+            ForEach(0..<12) { i in
+                Rectangle()
+                    .fill(Color.black)
+                    .frame(width: 5, height: 20)
+                    .offset(y: -150 + 10)
+                    .rotationEffect(.degrees(Double(i) * 30))
+            }
         }
     }
 }
